c1bd81f02b79720d9e7b13fd09ebe42b1895f8d7
Mariusz Bąk <mariusz.bak@xsolve.pl>
2016-09-30 13:25:06 +0200

    Step 1.

--- src/AppBundle/Command/AbstractTutorialCommand.php
+++ src/AppBundle/Command/AbstractTutorialCommand.php
@@ -5,6 +5,10 @@ declare(strict_types = 1);
 namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use RulerZ\Compiler\FileCompiler;
+use RulerZ\Compiler\Target;
+use RulerZ\Parser\HoaParser;
+use RulerZ\RulerZ;
 
 abstract class AbstractTutorialCommand extends ContainerAwareCommand
 {
@@ -12,5 +16,26 @@ abstract class AbstractTutorialCommand extends ContainerAwareCommand
     {
         $reflClass = new \ReflectionClass($this);
         $this->setName('step:'.substr(substr($reflClass->getShortName(), 0, -7), 4));
+    }
+
+    /**
+     * @return RulerZ
+     */
+    public function getRulerZ()
+    {
+        // Compiler
+        $compiler = new FileCompiler(
+            new HoaParser(),
+            $this->getContainer()->getParameter('kernel.cache_dir') . '/rulerz'
+        );
+
+        // RulerZ engine
+        $rulerz = new RulerZ(
+            $compiler, [
+                new Target\ArrayVisitor(),
+            ]
+        );
+
+        return $rulerz;
     }
 }
