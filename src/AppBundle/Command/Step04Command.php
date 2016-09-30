1b4d94b461ac1ee548de50bb67c663ebf459665d
Mariusz Bąk <mariusz.bak@xsolve.pl>
2016-09-30 13:43:57 +0200

    Step 4.

---
+++ src/AppBundle/Command/Step04Command.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use AppBundle\Entity\Unicorn;
+use AppBundle\Spec\AwesomeUnicorn;
+
+class Step04Command extends AbstractTutorialCommand
+{
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        $unicorns = [
+            new Unicorn('Klaus', 'white', false, true, true, false),
+            new Unicorn('Michael', 'blue', true, false, false, false),
+            new Unicorn('Sandy', 'pink', false, true, true, true),
+            new Unicorn('Mandy', 'pink', false, true, false, true),
+        ];
+
+        $spec = new AwesomeUnicorn();
+        $rulerz = $this->getRulerZ();
+        $awesomeUnicorns = $rulerz->filterSpec($unicorns, $spec);
+
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'Color', 'Has laser horn?', 'Poops rainbows?', 'Can fly?', 'Fluffy?']);
+
+        /** @var Unicorn $awesomeUnicorn */
+        foreach ($awesomeUnicorns as $awesomeUnicorn) {
+            $table->addRow($awesomeUnicorn->toArray());
+        }
+
+        $table->render();
+
+        $isAwesome = $rulerz->satisfiesSpec($unicorns[3], $spec);
+        $output->writeln('Fluffy ' . ($isAwesome ? 'is' : 'is not') . ' awesome.');
+    }
+
+}
---
+++ src/AppBundle/Spec/AwesomeUnicorn.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AppBundle\Spec;
+
+use RulerZ\Spec\AbstractSpecification;
+
+class AwesomeUnicorn extends AbstractSpecification
+{
+    public function getRule(): string
+    {
+        return 'canFly = true and poopsRainbows = true and color = :color'; // Problem - no completion for properties.
+    }
+
+    public function getParameters(): array
+    {
+        return [
+            'color' => 'white',
+        ];
+    }
+}
